553956152fd21d61048dc17629c73b262bb8c9d2
devsuthar777 <devsuthar777@gmail.com>
2024-08-01 10:57:07 +0530

    first commit

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import { CiSearch } from "react-icons/ci";
+
+const NavBar = () => {
+  return (
+    <div>
+        <img></img>
+        <div>
+            <div>
+                <CiSearch />
+                <input></input>
+            </div>
+            <ul>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+            </ul>
+        </div>
+    </div>
+  )
+}
+
+export default NavBar
