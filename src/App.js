bbbcddfd51c74cc9ea9f5445ce1979a552494327
devsuthar777 <devsuthar777@gmail.com>
2024-08-02 10:47:33 +0530

    orders and net profit cards done

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import logo from './logo.svg';
-import './App.css';
+
 import SideBar from './components/SideBar';
 import DashBoardPage from './components/DashBoardPage';
 import NavBar from './components/NavBar';
-
+import './App.css';
 function App() {
   return (
-    <div className='bg-richblue-700  h-screen w-11/12 mx-auto'>
+    <div className='bg-black  h-screen w-11/12 mx-auto'>
       <nav className='w-full'>
       <NavBar/>
       </nav>
-      <div className='w-full h-full'>
+      <div className='w-full h-full flex'>
         <SideBar/>
         <DashBoardPage/>
       </div>
--- src/components/DashBoardPage.jsx
+++ src/components/DashBoardPage.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
+import OrderStatics from './dashBoardComponents/OrderStatics'
+import NetProfit from './dashBoardComponents/NetProfit'
 
 const DashBoardPage = () => {
   return (
-    <div>DashBoardPage</div>
+    <div className='w-[92%] h-full mx-auto mt-2'>
+        <h1 className='font-bold text-white text-2xl py-2'>Dashbord</h1>
+        <div className='flex gap-5 w-full'>
+            <OrderStatics></OrderStatics>
+            <NetProfit></NetProfit>
+        </div>
+    </div>
   )
 }
 
